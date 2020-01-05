6f60a1a796f37ceafe77ac34899819fdbe82fdf6
mysticAxolotl <brighid1681@yahoo.com>
2020-01-04 19:09:09 -0500

    adding jump??

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -1,19 +1,24 @@
 using Godot;
 using System;
 
-public class Player : Area2D
+public class Player : KinematicBody2D
 {
     private Vector2 _velocity = new Vector2();
     [Export] public float MaxSpeed = 200f;
 
     [Export] public float Inertia = 10f;
-
     [Export] public float Friction = 10f;
+
+    [Export] public float gravity = 9.8f;
+
+    [Export] public float jump = 100f;
 
     public override void _Process(float delta)
     {
         var acceleration = new Vector2();
+        var horizontal = new Vector2();
 
+        acceleration.y = gravity;
         if (Input.IsActionPressed("ui_left"))
         {
             acceleration.x -= 1;
@@ -22,21 +27,28 @@ public class Player : Area2D
         {
             acceleration.x += 1;
         }
-        if (Input.IsActionPressed("ui_up"))
+        if(Input.IsActionPressed("ui_select"))
         {
-            acceleration.y -= 1;
+            horizontal.y -= jump;
         }
-        if (Input.IsActionPressed("ui_down"))
-        {
-            acceleration.y += 1;
-        }
+        else
+            horizontal.y += gravity;
         acceleration = acceleration.Normalized() * Inertia;
 
         _velocity = new Vector2(
-            x: Mathf.Clamp(_velocity.x + acceleration.x * delta, -MaxSpeed, MaxSpeed),
-            y: Mathf.Clamp(_velocity.y + acceleration.y * delta, -MaxSpeed, MaxSpeed)
+            x: Mathf.Clamp(_velocity.x + acceleration.x, -MaxSpeed, MaxSpeed),
+            y: Mathf.Clamp(_velocity.y + horizontal.y, -MaxSpeed, MaxSpeed)
         );
-
+        
+        // if( !IsOnFloor() )
+        // {
+        //     if( Input.IsActionPressed("ui_select") ) //Jump
+        //     {
+        //         _velocity.y -= jump;
+        //     }
+        // }
+        // else
+        //     _velocity.y += gravity;
         // TODO: Apply friction only when no keys are pressed
         var speed = _velocity.Length();
         if (_velocity.Length() != 0)
@@ -47,7 +59,8 @@ public class Player : Area2D
             var newSpeedRatio = newSpeed / speed;
             _velocity *= newSpeedRatio;
         }
+        
+        _velocity = MoveAndSlide( _velocity );
 
-        Position += _velocity;
     }
 }
