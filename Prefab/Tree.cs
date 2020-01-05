381ac202e8f0e138a7917b746036bd0e2179c36e
Patrick M. Howard <pmontgomeryhoward@gmail.com>
2020-01-04 21:32:43 -0500

    .\Prefab\Tree.cs

--- Prefab/Tree.cs
+++ Prefab/Tree.cs
@@ -5,19 +5,11 @@ using GameLogic;
 
 public class Tree : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
     [Export] public bool IsPlanted;
     [Export] public Texture TeamA_Texture;
     [Export] public Texture TeamB_Texture;
     [Export] public OriginatingTeam Team;
-
-
     
-
-    // Called when the node enters the scene tree for the first time.
     public void body_entered ( PhysicsBody2D body ){
 
         var parentType = body.GetParent();
